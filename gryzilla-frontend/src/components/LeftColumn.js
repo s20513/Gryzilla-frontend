317a6e1e523ce38217062802c1cbdeb65e30db13
s20660 <81235069+s20660@users.noreply.github.com>
2023-01-07 15:09:35 +0100

    code refactor

--- gryzilla-frontend/src/components/LeftColumn.js
+++ gryzilla-frontend/src/components/LeftColumn.js
@@ -48,10 +48,29 @@ export default function LeftColumn() {
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu className="dropdown-menu-right dropdown-menu-dark">
-                        <Dropdown.Item onClick={() => {setSortType('byDateDesc')}} active={sortType === "byDateDesc" ? true : false} href="#/action-2">Najnowsze</Dropdown.Item>
-                        <Dropdown.Item onClick={() => {setSortType("byDateAsc")}} active={sortType === "byDateAsc" ? true : false} href="#/action-3">Najstarsze</Dropdown.Item>
-                        <Dropdown.Item onClick={() => {setSortType('byLikesDesc')}} active={sortType === "byLikesDesc" ? true : false} href="#/action-1">Najpopularniejsze</Dropdown.Item>
-                        <Dropdown.Item onClick={() => {setSortType("byCommentsDesc")}} active={sortType === "byCommentsDesc" ? true : false} href="#/action-3">Najwięcej komentarzy</Dropdown.Item>
+                        <Dropdown.Item
+                            onClick={() => {setSortType('byDateDesc')}}
+                            active={sortType === "byDateDesc" ? true : false}
+                            href="#/action-2">
+                            Najnowsze
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                            onClick={() => {setSortType("byDateAsc")}}
+                            active={sortType === "byDateAsc" ? true : false}
+                            href="#/action-3">
+                            Najstarsze
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={() => {setSortType('byLikesDesc')}}
+                            active={sortType === "byLikesDesc" ? true : false}
+                            href="#/action-1">
+                            Najpopularniejsze
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                            onClick={() => {setSortType("byCommentsDesc")}}
+                            active={sortType === "byCommentsDesc" ? true : false}
+                            href="#/action-3">
+                            Najwięcej komentarzy
+                        </Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
             </Container>
