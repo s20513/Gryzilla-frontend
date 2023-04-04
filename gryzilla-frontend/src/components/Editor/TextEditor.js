2cf55ca50bc16a28df167d4ad5e881d3f2245207
s20660 <81235069+s20660@users.noreply.github.com>
2023-04-04 14:05:06 +0200

    editor

--- gryzilla-frontend/src/components/Editor/TextEditor.js
+++ gryzilla-frontend/src/components/Editor/TextEditor.js
@@ -1,55 +1,73 @@
 import React, { forwardRef, useImperativeHandle } from "react";
 import { useState } from "react";
 
-import { Editor } from 'react-draft-wysiwyg';
-import { convertToHTML } from 'draft-convert';
+import { Editor } from "react-draft-wysiwyg";
+import { convertToHTML } from "draft-convert";
 
 import {
-    EditorState,
-    convertToRaw,
-    ContentState,
-    convertFromHTML
-  } from "draft-js";
+	EditorState,
+	convertToRaw,
+	ContentState,
+	convertFromHTML,
+} from "draft-js";
 
+const TextEditor = forwardRef((props, _ref) => {
+	const initialContent = props.initialContent;
+	const placeHolder = props.placeHolder;
 
-const TextEditor =  forwardRef((props, _ref) =>{
+	const [editorState, setEditorState] = useState(() => {
+		if (initialContent != undefined) {
+			const blocksFromHTML = convertFromHTML(initialContent);
+			const contentState = ContentState.createFromBlockArray(
+				blocksFromHTML.contentBlocks,
+				blocksFromHTML.entityMap
+			);
+			return EditorState.createWithContent(contentState);
+		} else {
+			return EditorState.createEmpty();
+		}
+	});
 
-    const initialContent = props.initialContent;
-    const placeHolder = props.placeHolder;
+	useImperativeHandle(_ref, () => ({
+		getPostContent: () => {
+			return convertToHTML(editorState.getCurrentContent());
+		},
+	}));
 
-    const [editorState, setEditorState] = useState( () => {
-        if( initialContent != undefined){
-            const blocksFromHTML = convertFromHTML(initialContent)
-            const contentState = ContentState.createFromBlockArray(
-                blocksFromHTML.contentBlocks,
-                blocksFromHTML.entityMap
-            );
-            return EditorState.createWithContent(contentState)
-        } else {
-            return EditorState.createEmpty()
-        }
-    });
+	return (
+		<Editor
+			editorState={editorState}
+			onEditorStateChange={setEditorState}
+			placeholder={placeHolder}
+			toolbarClassName="toolbar-class"
+			wrapperClassName="wrapper-class"
+			editorClassName="editor-class"
 
-    useImperativeHandle(_ref, () => ({
-        getPostContent: () => {
-            return convertToHTML(editorState.getCurrentContent());
-        }
-    }));
-
-    return (
-        <Editor
-            editorState={editorState}
-            onEditorStateChange={setEditorState}
-            placeholder={placeHolder}
-            toolbarClassName="toolbar-class"
-            toolbar={{
-                options : ['inline'],
-                inline : {
-                     options : ['bold', 'italic', 'underline','strikethrough']
-                }
-            }}
-        />  
-    );
-})
+			toolbar={{
+				options: [
+					"inline",
+					"blockType",
+					"fontSize",
+					"fontFamily",
+					"list",
+					"textAlign",
+					"link",
+					"emoji",
+					"remove",
+					"history",
+				],
+				inline: {
+					options: [
+						"bold",
+						"italic",
+						"underline",
+						"strikethrough",
+						"superscript",
+					],
+				},
+			}}
+		/>
+	);
+});
 
 export default TextEditor;
